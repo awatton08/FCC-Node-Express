ecb74b16b23328eab35309a1453416d573c16182
awatton <a.watton08@gmail.com>
2021-04-14 05:59:15 -0400

    basic node and express 4

--- myApp.js
+++ myApp.js
@@ -3,7 +3,7 @@ var app = express();
 
 //console.log();
 
-app.use(__dirname + "/public", express.static())
+//app.use(__dirname + "/public", express.static())
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
